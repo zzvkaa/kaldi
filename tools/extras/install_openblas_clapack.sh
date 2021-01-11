1e2eeb9c95c68c6ba428b00bf12ccbd01cadde47
Nickolay Shmyrev <nshmyrev@gmail.com>
2021-01-11 12:58:19 +0100

    Set lowest target

--- tools/extras/install_openblas_clapack.sh
+++ tools/extras/install_openblas_clapack.sh
@@ -6,6 +6,6 @@ CLAPACK_VERSION=3.2.1
 git clone -b v${OPENBLAS_VERSION} --single-branch https://github.com/xianyi/OpenBLAS
 git clone -b v${CLAPACK_VERSION} --single-branch https://github.com/alphacep/clapack \
 
-make -C OpenBLAS ONLY_CBLAS=1 DYNAMIC_ARCH=1 USE_LOCKING=1 USE_THREAD=0 all
+make -C OpenBLAS ONLY_CBLAS=1 DYNAMIC_ARCH=1 TARGET=NEHALEM USE_LOCKING=1 USE_THREAD=0 all
 make -C OpenBLAS PREFIX=$(pwd)/OpenBLAS/install install
 mkdir -p clapack/BUILD && cd clapack/BUILD && cmake .. && make -j 10 && find . -name "*.a" | xargs cp -t ../../OpenBLAS/install/lib
