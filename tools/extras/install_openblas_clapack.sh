d88962dff9296787936ab512d542ad8ddee9984f
Nickolay Shmyrev <nshmyrev@gmail.com>
2021-10-10 22:30:51 +0200

    clapack fails on tests due to multiple definitions

--- tools/extras/install_openblas_clapack.sh
+++ tools/extras/install_openblas_clapack.sh
@@ -8,4 +8,8 @@ git clone -b v${CLAPACK_VERSION} --single-branch https://github.com/alphacep/cla
 
 make -C OpenBLAS ONLY_CBLAS=1 DYNAMIC_ARCH=1 TARGET=NEHALEM USE_LOCKING=1 USE_THREAD=0 all
 make -C OpenBLAS PREFIX=$(pwd)/OpenBLAS/install install
-mkdir -p clapack/BUILD && cd clapack/BUILD && cmake .. && make -j 10 && find . -name "*.a" | xargs cp -t ../../OpenBLAS/install/lib
+mkdir -p clapack/BUILD && cd clapack/BUILD && cmake .. \
+    && make -j 10 -C F2CLIBS \
+    && make -j 10 -C BLAS \
+    && make -j 10 -C SRC \
+    && find . -name "*.a" | xargs cp -t ../../OpenBLAS/install/lib
